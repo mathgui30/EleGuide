38ca8d5b32320cb9e584dc6047d27cc5a9ca94fa
AlvesOliveiraMatheus <matheusalv.oliveira@gmail.com>
2023-12-07 15:50:22 -0300

    add products implemented

---
+++ scripts/addProduct.js
@@ -0,0 +1,36 @@
+document.getElementById('adicionarProdutoBtn').addEventListener('click', function() {
+    const nome = document.getElementById('nomeProd').value;
+    const descricao = document.getElementById('descricao').value;
+    const preco = parseFloat(document.getElementById('preco').value);
+    const foto = document.getElementById('foto').value;
+  
+    const data = {
+      nome,
+      descricao,
+      preco,
+      foto
+    };
+  
+    fetch('http://localhost:3000/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro ao adicionar produto');
+      }
+      return response.json();
+    })
+    .then(result => {
+      console.log('Produto adicionado:', result);
+      // Lógica adicional, como fechar o modal ou atualizar a página
+    })
+    .catch(error => {
+      console.error('Erro:', error);
+      // Lógica para lidar com erros durante a requisição
+    });
+  });
+  
